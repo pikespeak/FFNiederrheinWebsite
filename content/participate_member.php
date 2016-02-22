6e90f27c6880510148b028598757d596e0cee38a
pikespeak <olivermark83@gmail.com>
2016-02-22 18:25:27 +0100

    Revert "Revert "Merge pull request #1 from pikespeak/FFNR-Firmware""
    This reverts commit a45703cf94dfc5abfee82058b7b256f2878cdd13.

--- content/participate_member.php
+++ content/participate_member.php
@@ -7,19 +7,34 @@
             <option value="1">Freifunk Firmware updaten</option>
         </select>
     </div>
-    <label>Wähle eine Community:</label>
+    <label>Wähle eine Stadt:</label>
     <div class="select-field">
         <select id="download_form_choose_comunity">
             <option value="0">Bitte auswählen</option>
-            <option value="1">Offizielle Community Firmware</option>
+            <option value="ffalpen">Alpen</option>
+			<option value="ffbgh">Bedburg Hau</option>
+			<option value="ffemm">Emmerich</option>
+			<option value="fffl">Offizielle Community Firmware</option>
+			<option value="ffgel">Geldern</option>
+			<option value="ffgoch">Goch</option>
+			<option value="ffhnx">Hünxe</option>
+			<option value="ffkev">Kevelaer</option>
+			<option value="ffkkar">Kalkar</option>
+			<option value="ffkle">Kleve</option>
+			<option value="ffkra">Kranenburg</option>
+            <option value="ffree">Rees</option>
+			<option value="ffstr">Straelen</option>
+			<option value="ffwawa">Wachtendonk/Wankum</option>
+			<option value="ffwes">Wesel</option>
+			<option value="ffwez">Weeze</option>
+			<option value="ffxan">Xanten</option>
             </select>
     </div>
     <label>Wähle einen Router aus:</label>
     <div class="select-field">
         <select id="download-form-router">
             <option value="-1">Bitte wähle einen Router aus</option>
-	    <option value="-allnet-all0315n">Allnet 0315n</option>
-            <option value="-buffalo-wzr-hp-ag300h-wzr-600dhp">Buffalo WZR HP AG300H WZR 600DHP</option>
+	        <option value="-buffalo-wzr-hp-ag300h-wzr-600dhp">Buffalo WZR HP AG300H WZR 600DHP</option>
             <option value="-buffalo-wzr-hp-g450h">Buffalo WZR HP G450H</option>
             <option value="-d-link-dir-615-rev-c1">D-Link 615 Rev:c1</option>
             <option value="-d-link-dir-825-rev-b1">D-Link 825 Rev:b1</option>
@@ -100,8 +115,10 @@
             <option value="-ubiquiti-unifi">Ubiquiti UniFi</option>
 	    <option value="-ubiquiti-unifi-ap-pro">Ubiquiti UniFi AP-Pro</option>
             <option value="-ubiquiti-unifiap-outdoor">Ubiquiti UniFi AP-Outdoor</option>
-	    <option value="-x86-generic">x86 Image für generische Prozessoren</option>
-	    <option value="-x86-kvm">x86 Image für KVM</option>
+	    <option value="-x86-generic.img.gz">x86 Image für generische Prozessoren</option>
+	    <option value="-x86-kvm.img.gz">x86 Image für KVM</option>
+        <option value="-x86-virtualbox.vdi">x86 Image für Virtualbox</option>
+        <option value="-x86-vmware.vmdk">x86 Image für VMWare</option>
         </select>
     </div>
     <button class="button">Herunterladen</button>
