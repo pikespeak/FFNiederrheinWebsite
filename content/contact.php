33e903bcfd3ffd3aa52e6185cd05cbf133066e03
pikespeak <olivermark83@gmail.com>
2016-04-08 13:29:46 +0200

    Kontakt angepasst

--- content/contact.php
+++ content/contact.php
@@ -2,10 +2,12 @@
 	<h3>Offline</h3>
 	<p>Freifunk Niederrhein ist ein Community mit Unterstützung des Freifunk Rheinland e. V. &ndash; Interessierte sind gerne eingeladen bei den Treffen des Freifunk Niederrein welches in den Teilnehmenden Städten stattfindet vorbei zu schauen.<br></p>
 	<address>
-		Telefon: <a href="tel:0461574945882">0461 574945882</a>  <br>
-		Fax: <a target="_blank" href="https://twitter.com/pikespeak_marol/status/700430941790081025">Faxnummer?<br>
-		Zur Bleiche 12 <br>
-		24941 Flensburg
+	Christian Nowak<br>
+	Am Gieselberg 128<br>
+	47638 Straelen<br>
+	Telefon: <a href="tel:0151 20081609">0151 20081609</a>
+
+
 	</address>
 </div>
 <div class="contact_right">
@@ -14,6 +16,8 @@
 		E-Mail-Adresse: <a target="_blank" href="mailto:info@freifunk-niederrhein.de">info@freifunk-niederrhein.de</a><br>
 		Facebook: <a target="_blank" href="https://www.facebook.com/freifunkniederrhein">facebook.com/FreifunkNiederrhein</a><br>
 		Twitter: <a target="_blank" href="https://twitter.com/FFNiederrhein">@FFNiederrhein</a><br>
-		Slack: <a target="_blank" href="https://freifunkniederrhein.slack.com">#Freifunk-Niederrhein-Chat</a><br>
+		Slack: <a target="_blank" href="https://freifunkniederrhein.slack.com">#Freifunk-Niederrhein-Chat</a><br><br>
+		Derzeit nutzten wir hauptsächlich Slack, anmelden kannst du dich <a target="_blank" href="http://slackin-freifunkniederrhein.herokuapp.com">hier</a> <script async defer src="http://slackin-freifunkniederrhein.herokuapp.com/slackin.js?large"></script>
+		<br>
 		</address>
 </div>
